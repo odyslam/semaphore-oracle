5451ac038057f8fd597706e40a35c3f9468c069b
Odysseas.eth <odyslam@gmail.com>
2023-04-30 23:54:16 +0200

    feat: deploy script

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -2,21 +2,63 @@
 pragma solidity 0.8.4;
 import {Script} from "forge-std/Script.sol";
 import {ZuzaluOracle} from "../src/ZuzaluOracle.sol";
+import {SemaphoreVerifier} from "@semaphore-protocol/contracts/base/SemaphoreVerifier.sol";
+import {console2 as console} from "forge-std/console2.sol";
 
-contract Deploy {
-  function deployOracle(address verifier) public {
-    ZuzaluOracle oracle = new ZuzaluOracle({
-      _owner: msg.sender,
-      _verifier: verifier
-    });
+bytes32 constant SALT = "Zuzalu #1 2023";
+address constant DEPLOYED_VERIFIER= 0x3889927F0B5Eb1a02C6E2C20b39a1Bd4EAd76131;
+contract Deploy is Script{
+
+  address $verifier;
+  address $owner;
+
+  constructor() {
+    if (block.chainid == 5 || block.chainid == 11155111 || block.chainid == 80001 || block.chainid == 10 || block.chainid == 421613){
+      $verifier = DEPLOYED_VERIFIER;
+      console.log("Verifier is already deployed at address", $verifier);
+    } else {
+      _deployVerifier();
+      console.log("Verifier deployed at address", $verifier);
+    }
+  }
+  function run() public {
+    $owner = msg.sender;
+    deployOracle();
+  }
+  
+  function run(address _verifier) public {
+    $owner = msg.sender;
+    $verifier = _verifier;
+    deployOracle();
   }
 
-  function deployLottery(address oracle, address recipient, uint256 durationInDays) public {
-    Lottery lottery = new Lottery({
-      _oracle: oracle,
-      _end: block.timestamp + (durationInDays days),
-      _recipient: recipient,
-      _baseURI: "https://zuzalu-lottery.odyslam.workers.dev/nft/"
+  function _deployVerifier() internal {
+    vm.startBroadcast();
+    SemaphoreVerifier verifierContract = new SemaphoreVerifier();
+    $verifier = address(verifierContract);
+    vm.stopBroadcast();
+  }
+
+  function deployOracle() internal {
+    vm.startBroadcast();
+    ZuzaluOracle oracle = new ZuzaluOracle{salt: SALT}({
+      _owner: $owner,
+      _verifier: $verifier 
     });
+    vm.stopBroadcast();
+    console.log("Oracle deployed at address", address(oracle));
+  }
+
+  function printAddress(uint256 _chainId, address owner) public {
+    vm.chainId(_chainId);
+    bytes memory args = abi.encode(owner, $verifier);
+    bytes32 initCodeHash = hashInitCode(type(ZuzaluOracle).creationCode, args);
+    address oracle = computeCreate2Address(SALT, initCodeHash);
+    console.log("Create2 Args");
+    console.log("> Owner:  ", owner);
+    console.log("> Verifier", $verifier);
+    console.log("> Salt", vm.toString(SALT));
+    console.log("Address");
+    console.log("> Oracle: ", oracle);
   }
 }
